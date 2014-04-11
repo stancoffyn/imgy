a837afe5071785a50aaa07124b9f5acb6def8387
Matthew L. Houck <mhouck@godaddy.com>
2014-04-11 00:22:43 -0700

    Added capability to edit images.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Testsite::Application.routes.draw do
+  get "image/edit"
+  get "image/show"
+  get "image/index"
   resources :tags
   resources :upload
-
+  resources :image
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
